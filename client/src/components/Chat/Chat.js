1e4a1c1c9c930a8d6b66d806ba0e62281bccce43
marasinipradeep <marasinipradeep@gmail.com>
2020-09-15 12:57:13 +0930

    #Join UI completed with necessary css

---
+++ client/src/components/Chat/Chat.js
@@ -0,0 +1,13 @@
+import React,{useState,useEffect} from 'react';
+import queryString from 'query-string';
+import io from 'socket.io-client';
+
+
+
+export default function Chat() {
+
+    //The use effect is hook that lets you perform side effects function components
+    return (
+       <h1>Chat</h1>
+    )
+}
---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Join.css'
+
+export default function Join() {
+
+    const [name, setName] = useState('');
+    const [room, setRoom] = useState('');
+
+
+    return (
+        <div className="joinOuterContainer">
+            <div className="joinInnerContainer">
+                <h1 className="heading">Join</h1>
+                <div><input placeholder="Name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)}></input></div>
+                <div><input placeholder="Room" className="joinInput" type="text" onChange={(event) => setRoom(event.target.value)}></input></div>
+                <Link onClick={event=>(!name||!room)?event.preventDefault():null} to={`/chat?name=${name}&room=${room}`}>
+                    <button className="button mt-20" type="submit">Sign In</button>
+                </Link>
+            </div>
+
+        </div>
+
+    )
+}
